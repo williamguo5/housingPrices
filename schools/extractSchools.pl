8f098a1aff6dabecc71c5999ebdd73982061f87d
Sam Wemyss <your_email@youremail.com>
2015-09-07 16:12:07 +1000

    Updated extractSchools to seperate TYPE and insert ranks for highschools

--- schools/extractSchools.pl
+++ schools/extractSchools.pl
@@ -1,48 +1,74 @@
 #!/usr/bin/perl -w
+use Switch
+
+my $total = 0;
 open (CSV, ">schools.txt") or die "Can't create/open schools file";
 foreach my $file (glob "pages/*.html") {
 	open (F, "<$file") or die "Can't open file.";
 	$suburbName = $file;
 	$suburbName =~ s/.*?\///;
 	$suburbName =~ s/\.html//;
-
 	my $addressFlag = FALSE;
 	my $typeFlag = FALSE;
 	my $schoolFlag = FALSE;
+	my $descFlag = FALSE;
 	my $school = "";
 	my $address = "";
 	my $type = "";
+	my $description = "";
+
+	
+
 	while (<F>) {
 		if (m/There was an error locating the location you chose/i) {
+			#print "WHERE IS THIS: $suburbName\n";
 			last;
 		}
 		# If no results are found, skip file (as there are nearby schools in other suburbs)
 		if (m/Returned (\d+)/i) {
 			if ($1 eq "0") {
+				#print "NO SCHOOLS HERE: $suburbName\n";
 				last;
 			}
 		}
-		# School name
-		#f (m/<a[^>]+?href="\/\d+\/[a-zA-Z-_]+?">.*?([a-zA-Z_\-\ ',\(\)&6]+)<\/a>/) {
+		##### Name #####
 		if ($schoolFlag eq FALSE && m/<a[^>]+?href="\/\d+\/.*>(.*)<\/a>$/) {
 			$school = $1;
 			$schoolFlag = TRUE;
+			next;
 		}
 
-		# School address 
-		
+		##### Address #####
 		if ($addressFlag eq TRUE) {
 			chomp $_;
 			$address .= $_;
 			if (m/<\/p>/) {
 				$addressFlag = FALSE;
 			}
+			next;
 		}
 		if (m/<p class="address">/) {
 			$addressFlag = TRUE;
+			next;
 		}
 
-		# School category (Primary, High, Catholic, Private)
+		##### Description #####
+		if ($descFlag eq TRUE) {
+			chomp $_;
+			
+			$description = "$_";
+			$description =~ s/<.*$//;
+			$description =~ s/^\s+//;
+			$description =~ s/\s+$//;
+			$descFlag = FALSE;
+			next;
+		}
+		if (m/shortdescription/) {
+			$descFlag = TRUE;
+			next;
+		}
+
+		##### School category (Primary, High, Catholic, Private) #####
 		if ($typeFlag eq TRUE) {
 			chomp $_;
 			$type .= $_;
@@ -53,12 +79,61 @@ foreach my $file (glob "pages/*.html") {
 				$type =~ s/\s+/ /g;
 				$type =~ s/<\/li> <li>/, /g;
 				$type =~ s/<[^>]*?>//g;
-				$type =~ s/^\s+//;
-				$type =~ s/\s+$//;
+				$type =~ s/\s+//;
 				$address =~ s/\s+/ /g;
 				$address =~ s/<br>/,/g;
 				$address =~ s/\s+<\/p>\s*//;
 				$address =~ s/^\s+//;
+				($street = $address) =~ s/^([^,]+).*/$1/;
+				($postcode = $address) =~ s/.*(\d{4})$/$1/;
+				($state = $address) =~ s/.*([A-Z]{3}).*/$1/;
+				my $government = FALSE;
+				my $primary = FALSE;
+				my $secondary = FALSE;
+				my $religion = "";
+				my $gender = "";
+				if ($type =~ m/government/i) {
+					$government = TRUE;
+				}
+				if ($type =~ m/primary/i) {
+					$primary = TRUE;
+				}
+				if ($type =~ m/secondary/i) {
+					$secondary = TRUE;
+				}
+
+				if ($type =~ m/([a-z]+\-denominational|anglican|catholic|christian|islamic|jewish|presbyterian|seventh day adventist|uniting church|[a-z]+ Orthodox|baptist|church of england|assyrian|multi-faith)/i) {
+					$religion = $1;
+				}
+
+				if ($type =~ m/(co[\-]?ed|girls|boys)/i) {
+					$gender = $1;
+					$gender =~ s/\-//g;
+				}
+
+				open (HIGHSCHOOL, "<schoolRanks.html") or die "Can't open schoolRanks.html";
+				my $rank = 0;
+				my $rankFlag = FALSE;
+				(my $tempSchoolName = $school) =~ s/\(.*\)//;	# NEED TO IGNORE WHITESPACE
+				$tempSchoolName =~ s/\s+//g;
+				while (my $line = <HIGHSCHOOL>) {
+					if ($line =~ m/>(.{4,})<\/a>/) {
+						$rank += 1;
+						my $name = $1;
+						$name =~ s/\(.*\)//;
+						$name =~ s/\s+//g;
+						if (lc($name) eq lc($tempSchoolName)) {
+							$rankFlag = TRUE;
+							$total += 1;
+							last;
+						}
+					}
+				}
+				if ($rankFlag eq FALSE) {
+					$rank = -1;
+				}
+				close HIGHSCHOOL;
+
 				if ($address =~ m/NSW/) {
 					# Print line to file
 					if ($school eq "") {
@@ -70,26 +145,27 @@ foreach my $file (glob "pages/*.html") {
 					if ($address eq "") {
 						print "NO $suburbName\n"
 					}
-					print CSV "$suburbName|$school|$address|$type\n";
+					print CSV "$rank|$school|$government|$primary|$secondary|$religion|$gender|$street|$suburbName|$state|$postcode|$description\n";
 				}
 				$typeFlag = FALSE;
 				$addressFlag = FALSE;
 				$schoolFlag = FALSE;
+				$descFlag = FALSE;
 				
 				# Reset variables
 				$school = "";
 				$address = "";
 				$type = "";
+				$description = "";
 			}
 		}
 		if (m/<div class="listing-section attributes">/) {
 			$typeFlag = TRUE;
+			next;
 		}
 	}
 	
 	close F;
 }
 close CSV;
-#foreach my $file (glob "schools/*.txt") {
-
-#}
+print "$total\n";
