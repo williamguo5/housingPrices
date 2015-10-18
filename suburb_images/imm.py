21249efddadd4058053f544432d8b51f2f0b837c
Nethan Tri Hien Tran <z5016586@cse.unsw.EDU.AU>
2015-10-18 20:56:25 +1100

    Edited imm.py don't run, slow slow

--- suburb_images/imm.py
+++ suburb_images/imm.py
@@ -1,15 +1,13 @@
-import urllib2, urllib
-import simplejson, json
-import cStringIO
+import urllib, urllib2
+import json
 import re, csv
-from PIL import Image
 import time
 
 # output array
 output = []
 
 # get file with data name
-with open('suburbNames.txt') as f:
+with open('../Data/suburbNames.txt') as f:
     lines = f.readlines()
 
 for items in lines:
@@ -17,34 +15,39 @@ for items in lines:
 
 	suburb = items.rstrip('\n')
 	# status
-	print 'adding : '+suburb
+	print ('adding : '+suburb)
 	new_list.append(suburb)
 	suburb = re.sub(' ', '+', suburb)
 
 	fetcher = urllib2.build_opener()
-	searchTerm = suburb+'+SYDNEY'
+	searchTerm = suburb+'+AND+SYDNEY+site:domain.com.au'
 	startIndex = 0
+	index = 0
 	refresh = 0
 
-	searchUrl = "http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=" + searchTerm + "&start=" + str(startIndex)
-	f = fetcher.open(searchUrl)
-	deserialized_output = simplejson.load(f)
-	while startIndex < 4 and refresh < 20:
-		try:
-			imageUrl = deserialized_output['responseData']['results'][startIndex]['unescapedUrl']
-			#file = cStringIO.StringIO(urllib.urlopen(imageUrl).read())
-			#img = Image.open(file)
-			#print imageUrl
-			new_list.append(str(imageUrl))
-			startIndex += 1
+	while startIndex < 5:
+		searchUrl = "http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=" + searchTerm + "&start=" + str(startIndex) + "&imgtype=photo"
+		f = fetcher.open(searchUrl)
+		deserialized_output = json.load(f)
+		while index < 4 and refresh < 20:
+			try:
+				imageUrl = deserialized_output['responseData']['results'][index]['unescapedUrl']
+				#file = cStringIO.StringIO(urllib.urlopen(imageUrl).read())
+				#img = Image.open(file)
+				#print imageUrl
+				new_list.append(str(imageUrl))
+				index += 1
 
-		except:
-			time.sleep(20)
-			f = fetcher.open(searchUrl)
-			deserialized_output = simplejson.load(f)
-			refresh += 1
-			if refresh == 20:
-				new_list.append('empty')
+			except:
+				time.sleep(20)
+				f = fetcher.open(searchUrl)
+				deserialized_output = json.load(f)
+				refresh += 1
+				if refresh == 20:
+					new_list.append('empty')
+		index = 0
+		refresh = 0
+		startIndex += 4
 
 
 
